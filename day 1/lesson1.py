edd9983d78f6465c3a0c82a68f1c54540799299e
Ghosteman1 <dtxaratishvili.2002@gmail.com>
2023-09-08 00:53:12 +0400

    first commit

---
+++ day 1/lesson1.py
@@ -0,0 +1,10 @@
+name = "I'am Demetre"
+surname = "Kharatishvili,"
+letters = "I am"
+age = 21
+letters1 = "years old, and my height is"
+height = 177
+letters2 = ".I live in Rustavi. I love sport, and i am often busy with workout. I also love programming, I am interested in this profession and I want to learn more and more."
+I_love_programming = True
+is_bad = False
+print (name, surname, letters, str(age), letters1, height, letters2, I_love_programming, is_bad)
